89914ef41358d9f46eb6c77f5aef9c7b1e40e046
usualpro <usualpro@gmail.com>
2021-01-25 14:08:32 +0100

    Update dogs.js

--- pages/dogs.js
+++ pages/dogs.js
@@ -1,11 +1,16 @@
-import Link from 'next/link';
 
-export default function Dogs({breeds}) {
-    return <h1>HELLO DOGS {JSON.stringify(breeds)}</h1>
+export default function Dogs({ breeds }) {
+    const List = () => <ul>
+        {
+            Object.keys(breeds).map((dogName, index) => <li>{dogName}</li>)
+        }
+    </ul>
+    return <div>
+        <List />
+    </div>
 }
-
 Dogs.getInitialProps = async (ctx) => {
     const res = await fetch('https://dog.ceo/api/breeds/list/all')
     const json = await res.json();
     return { breeds: json.message }
-} 
+}
